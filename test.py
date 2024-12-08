1dfd8552d6cac66a742283628b5eb43bcf88d59c
Rounak <150204832+rounaksarak@users.noreply.github.com>
2024-12-08 22:35:13 +0530

    more data

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ api_request_count = 0
 total_moves_explored = 0  # Total number of moves explored
 initial_moves = ['e2e4','e7e5']  # Initial moves to start the opening repertoire
 iam = 1  # 1 for white, 0 for black
-requiredGames = 100  # Minimum number of games required for a move to be considered
+requiredGames = 10  # Minimum number of games required for a move to be considered
 
 
 
@@ -101,7 +101,7 @@ def get_opponent_moves(moves):
 
 def build_opening_repertoire(board, moves, repertoire=None):
     global total_moves_explored
-
+    print('Total Moves Explored',total_moves_explored)
     if repertoire is None:
         repertoire = []
 
