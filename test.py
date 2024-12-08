2e65faf3730ba7ec29fb6181e4f3b1643179d97f
Rounak <150204832+rounaksarak@users.noreply.github.com>
2024-12-08 22:42:41 +0530

    datas

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ requiredGames = 10  # Minimum number of games required for a move to be consider
 
 
 # Configure logging
-logging.basicConfig(level=logging.WARNING, format='%(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 
 # Initialize Stockfish
 stockfish = Stockfish(path="C:\\Apps\\stockfish\\stockfish-windows-x86-64-avx2.exe")
